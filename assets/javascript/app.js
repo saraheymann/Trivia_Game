ddef9520ba51af0ba21ed637287026e1c4b99846
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-05-31 11:56:32 -0500

    question set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(".startButton").click(function(){
+    // fixed?
+    // make it disappear
+    // starts the game and counter
+});
+// right answers
+var question1 = ($("input[@name=q1]:checked").val() != "c");
+var question2 = ($("input[@name=q2]:checked").val() != "d");
+var question3 = ($("input[@name=q3]:checked").val() != "a");
+var question4 = ($("input[@name=q4]:checked").val() != "b");
+var question5 = ($("input[@name=q5]:checked").val() != "b");
+var question6 = ($("input[@name=q6]:checked").val() != "c");
+var question7 = ($("input[@name=q7]:checked").val() != "d");
+var question8 = ($("input[@name=q8]:checked").val() != "a");
+var question9 = ($("input[@name=q9]:checked").val() != "b");
+var question10 = ($("input[@name=q10]:checked").val() != "d");
+var question11 = ($("input[@name=q11]:checked").val() != "a");
